1d2293cbf7158c9844d4c93df0e7b4029f7a29b7
westshae <shaewest02@gmail.com>
2023-10-22 16:58:31 +1300

    Removed utility file and added to auth service

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -6,7 +6,6 @@ import * as nodemailer from "nodemailer";
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import "dotenv/config";
-import { checkEmail, checkToken } from "src/utility/sanitise";
 
 @Injectable()
 export class AuthService {
@@ -14,7 +13,7 @@ export class AuthService {
   private readonly authRepo: Repository<AuthEntity>;
 
   async sendCode(email: string) {
-    if(!checkEmail(email)) return false;
+    if(!this.checkEmail(email)) return false;
     if ((await this.authRepo.findOne({ email: email })) === undefined) {
       this.registerAccount(email);
     }
@@ -33,7 +32,7 @@ export class AuthService {
   }
 
   async registerAccount(email: string) {
-    if(!checkEmail(email)) return false;
+    if(!this.checkEmail(email)) return false;
     this.authRepo.insert({
       email: email,
     });
@@ -106,4 +105,32 @@ export class AuthService {
       }
     });
   }
+
+  checkEmail(email:string){
+    let emailRegex =
+    /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/;
+  
+    if (email.match(emailRegex) === null) return false;
+    return true;
+  }
+  
+  checkToken(email:string, token:string){
+    if(!this.checkEmail(email)) return false;
+    try {
+      const decoded = jwt.verify(token, process.env.PRIVATEKEY) as jwt.JwtPayload;
+      console.log(decoded);
+      if(decoded === null){
+        return false;
+      }else {
+        if(decoded.email != email){
+          return false;
+        }
+        return true;
+      }
+    }catch(e){
+      console.error(e);
+      return false;
+    }
+  }
+  
 }
