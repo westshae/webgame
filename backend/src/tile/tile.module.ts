48d89af80fcaf8f823b15680bce13a8cbbbbf08f
westshae <shaewest02@gmail.com>
2023-11-13 23:05:15 +1300

    removed tile controller

--- backend/src/tile/tile.module.ts
+++ backend/src/tile/tile.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TileEntity } from './tile.entity';
 import { TileService } from './tile.service';
-import { TileController } from './tile.controller';
 import { StateEntity } from 'src/state/state.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TileEntity, StateEntity])],
-  controllers: [TileController],
   providers: [TileService]
 })
 export class TileModule {}
